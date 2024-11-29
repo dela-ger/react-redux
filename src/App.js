144db3d601a9b34d112292769496c6dd87df9a27
gdjamehkt <gdjameh@ktdevs.com>
2024-11-29 10:36:35 +0000

    Added set count function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./store";
+import { increment, decrement, reset, setCount } from "./store";
 
 function App() {
   const count = useSelector( (state) => state.count)
   const dispatch = useDispatch()
+  const [inputValue, setInputValue] = useState(0)
+
+  const handleSetCount = () => { dispatch(setCount(Number(inputValue))); };
 
   return (
     <>
@@ -12,6 +15,10 @@ function App() {
 
       <button onClick={() => dispatch(increment())}>Increment</button>
       <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(reset())}>Reset</button><br />
+      <input type="number" value={inputValue} 
+      onChange={(e) => setInputValue(e.target.value)} /> 
+      <button onClick={handleSetCount}>Set Count</button>
     </>
   );
 }
--- src/store.js
+++ src/store.js
@@ -7,10 +7,12 @@ const initialState = {
 // create action type
 const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
+const RESET = 'RESET';
+export const SET_COUNT = 'SET_COUNT'
 
 // action creator
 
-export const increment = () => ({  type: INCREMENT });
+export const increment = () => ({ type: INCREMENT });
 
 export const decrement = () => {
     return {
@@ -18,13 +20,22 @@ export const decrement = () => {
     }
 };
 
+export const reset = () => ({ type: RESET })
+export const setCount = (value) => ({ type: SET_COUNT, payload: value })
+
 // create the reducer function
 const counterReducer = (state = initialState, action) => {
     if (action.type === INCREMENT) {
         return { ...state, count: state.count + 1 }
     } else if (action.type === DECREMENT) {
         return { ...state, count: state.count - 1 }
-    }else {
+    }else if (action.type === RESET) {
+        return { ...state, count: 0 }
+    }else if (action.type === SET_COUNT) {
+        return { ...state, count: action.payload }
+    }
+    
+    else {
         return state
     }
 };
