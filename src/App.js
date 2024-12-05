72982e9d885a8d9f1b7803c9edb8444d5cb32e48
gdjamehkt <gdjameh@ktdevs.com>
2024-12-05 14:28:03 +0000

    Added redux thunk

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement, reset, setCount } from "./store";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchData } from "./store";
 
 function App() {
-  const count = useSelector( (state) => state.count)
   const dispatch = useDispatch()
-  const [inputValue, setInputValue] = useState(0)
+  const { loading, data, error } = useSelector((state) => state)
 
-  const handleSetCount = () => { dispatch(setCount(Number(inputValue))); };
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]) 
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error: {error}</p>
 
   return (
     <>
-      <h1>Count: {count}</h1>
-
-      <button onClick={() => dispatch(increment())}>Increment</button>
-      <button onClick={() => dispatch(decrement())}>Decrement</button>
-      <button onClick={() => dispatch(reset())}>Reset</button><br />
-      <input type="number" value={inputValue} 
-      onChange={(e) => setInputValue(e.target.value)} /> 
-      <button onClick={handleSetCount}>Set Count</button>
+      <div>
+        <h1>Fetched Data</h1>
+        <ul>
+          {data.map(item => {
+            <li key={item.id}>{item.name}</li>
+          })}
+        </ul>
+      </div>
     </>
   );
 }
--- src/store.js
+++ src/store.js
@@ -1,2 +1,40 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from 'redux-thunk';
+import rootReducer from "./reducers";
+
+export const fetchData = () => {
+    return async (dispatch) => {
+        dispatch({ type: "FETCH_DATA_REQUEST" });
+
+        try {
+           const response = await fetch("https://dummyjson.com/products") 
+           const data = await response.json();
+           dispatch({ type: "FETCH_DATA_SUCCESS", payload: data })
+        } catch (error){
+            dispatch({ type: "FETCH_DATA_FAILURE", payload: error.message })
+        }
+    }
+}
+
+const initialState = {
+    loading: false,
+    data: [],
+    error: null,
+}
+
+const dataReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "FETCH_DATA_REQUEST":
+            return { ...state, loading: true, error: null  }
+        case "FETCH_DATA_SUCCESS":
+            return { ...state, loading: false, data: action.payload }
+        case "FETCH_DATA_FAILURE":
+            return {...state, loading: false, error: action.payload}
+        default:
+            return state;
+    }
+}
+
+const store = createStore(dataReducer, applyMiddleware(thunk))
+
+export default dataReducer;
